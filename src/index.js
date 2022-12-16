9bdcd88bb12f4fad11702ded5657ce5dc21ef3a5
Ramon Meza <ramon_meza@live.com>
2022-12-16 17:28:58 -0500

    Add some placeholder content

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Ramon is cool.</h1>);
