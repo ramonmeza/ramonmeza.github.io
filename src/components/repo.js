df68a8e498c31e104ceeadfdf9d8e0cc0c62a024
Ramon Meza <ramon_meza@live.com>
2024-07-16 20:42:30 -0400

    Styling

--- src/components/repo.js
+++ src/components/repo.js
@@ -1,9 +1,9 @@
 function Repo({ url, name, description }) {
     return (
         <a href={url}>
-            <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div class="my-6 p-6 max-w-md mx-auto bg-white rounded-xl shadow-md">
                 <div class="text-xl font-medium text-black">{name}</div>
-                <p class="text-gray-500">{description}</p>
+                <p class="mt-2 text-gray-500">{description}</p>
             </div>
         </a>
     );
